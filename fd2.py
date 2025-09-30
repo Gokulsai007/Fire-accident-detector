8eb67b8cc24715daac48e8200d19653507a6df33
Gokulsai007 <gokulsiddhartha7@gmail.com>
2025-09-30 23:07:50 +0530

    Add files via upload

---
+++ fd2.py
@@ -0,0 +1,30 @@
+import cv2
+import pygame
+from mail import send_email
+fire_cascade = cv2.CascadeClassifier('fire_detection.xml')
+a=False
+pygame.mixer.init()
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to grab frame or end of video reached.")
+        break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    fire = fire_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=4)
+    if len(fire) > 0:
+        for (x, y, w, h) in fire:
+            cv2.rectangle(frame, (x-10, y-10), (x+w+10, y+h+10), (255, 0, 0), 3)
+        if not a:
+            print("fire is detected")
+            pygame.mixer.Sound("alarm.wav").play()
+            a = True
+            send_email()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
